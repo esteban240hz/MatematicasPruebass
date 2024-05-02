68994d5e7ba13d3466aaadbc97fe591ed48180be
esteban240hz <esteban240hz@.com>
2024-05-02 15:14:16 -0500

    sada

--- Operaciones/obj/Debug/net8.0/operaciones.AssemblyInfo.cs
+++ Operaciones/obj/Debug/net8.0/operaciones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("operaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb857d03785dfce942f9f756616d3ce3e6aa6e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24ff2a0782ce7b612b643f395800149808f5a0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("operaciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("operaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
