4f1db9181abb16b634baea5bc8b0e7927c5349b7
esteban240hz <esteban240hz@.com>
2024-05-02 13:54:42 -0500

    Commit

---
+++ Operaciones/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Operaciones
+{
+    public class OperacionesBasicas
+    {
+        // función para sumar
+        public int Suma(int a, int b)
+        {
+            return a + b; // retorna el resultado de la suma
+        }
+
+        // función para multiplicar
+        public int Multiplicacion(int a, int b)
+        {
+            return a * b; // retorna el resultado de la multiplicación
+        }
+    }
+}
